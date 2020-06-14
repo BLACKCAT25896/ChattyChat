c366df95af93fc82f78a633f97079e5244d8dd2d
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2020-06-14 20:46:36 +0600

    config api

--- Smack/Controller/CreateAccount.swift
+++ Smack/Controller/CreateAccount.swift
@@ -12,7 +12,6 @@ class CreateAccount: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
     
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,7 +7,27 @@
 //
 
 import Foundation
+
+typealias CompletionHandler = (_ Success : Bool) -> ()
+
+
+//url constants
+
+let BASE_URL = "https://chattychatjoy.herokuapp.com/v1"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+//segue
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWINT_TO_CHANNEL = "unwindtohannel"
+
+//user defaults
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token_key"
+let USER_EMAIL = "userEmail"
+
+
+
+
 
